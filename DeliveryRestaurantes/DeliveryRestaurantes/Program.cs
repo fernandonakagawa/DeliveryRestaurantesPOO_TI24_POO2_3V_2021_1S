c74cdfb058219f9a341ee536517389ffcb796c8a
fernandonakagawa <fernando.nakagawa@sesisenaipr.org.br>
2021-03-23 14:41:47 -0300

    Projeto criado

---
+++ DeliveryRestaurantes/DeliveryRestaurantes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeliveryRestaurantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
